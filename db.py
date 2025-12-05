66fc865a83db8034c3573b7da89228c397d8d0ed
tyler <murftyler2007@gmail.com>
2025-12-04 21:57:59 -0330

    csv file

---
+++ db.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+FILENAME"money.txt"
+
+def read_money():
+	if not os.path.exists(FILENAME):
+		return 100.0
+
+	try:
+		with open(FILENAME, "r", newline"") as file:
+			reader = csv.reader(file)
+			row = next(reader, None)
+			if row:
+				return float(row[0])
+	except Exception:
+		return 100.0
+
+def money_to_txt(amount):
+	try: 
+		with open(FILEOPEN, "w", newline="") as file:
+			writer = csv.writer(file)
+			writer.writerow([round(amount,2)])
+	except Exception as e:
+		print("error writing file:")
