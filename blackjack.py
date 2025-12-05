d9ea0aeeae76ca5824ab491892654d8922695c5b
tyler <murftyler2007@gmail.com>
2025-12-04 21:38:53 -0330

    blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -3,15 +3,16 @@ import random
 def whole_deck():
 	suits=["hearts","diamonds","clubs","spades"] 
 	numbered_cards = [
-		("2", 2) ("3", 3) ("4", 4) ("5", 5) ("6", 6)
-		("7", 7) ("8", 8) ("9", 0) ("10", 10)
-		("jack", 10) ("queen", 10) ("king", 10)
+		("Ace", 11),
+		("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6),
+		("7", 7), ("8", 8), ("9", 0), ("10", 10),
+		("jack", 10), ("queen", 10), ("king", 10)
 	]
 
 	deck=[]
 	for suit in suits:
 		for numbered_card, value in numbered_cards:
-			deck.append([numbered_card,suit,vlaue])
+			deck.append([numbered_card,suit,vaule])
 	return deck
 
 def hand_number(hand):
@@ -28,8 +29,21 @@ def hand_number(hand):
 		value -=10
 		aces -= 1
 	return value
+def in_hand(hand):
+	text=" "
+	
+
 def main():
+	print("blackjack")
+	
+	money=read_money
+	print(f"Money: {money.2f}")
+
+	deck = whole_deck
+	player = [deck.pop(). deck.pop()]
+	dealer = [deck.pop(), deck.pop()]
 
 
-if __name__ == "__maiun__":
+
+if __name__ == "__main__":
 	main()
